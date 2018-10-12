c738f0ca35728f4203d109e04648548946bd5e67
shravan318 <shravan.318@gmail.com>
2018-10-12 18:25:35 +0530

    Updates

---
+++ precache-manifest.170c0510c6b37e2282a88018ff1c1efe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "00bb9ac972d8874b806c",
+    "url": "/Contact-Manager-React/static/js/runtime~main.00bb9ac9.js"
+  },
+  {
+    "revision": "a066f6a21bcdfadf6269",
+    "url": "/Contact-Manager-React/static/js/main.a066f6a2.chunk.js"
+  },
+  {
+    "revision": "a1992afb8ea0c927db08",
+    "url": "/Contact-Manager-React/static/js/1.a1992afb.chunk.js"
+  },
+  {
+    "revision": "a066f6a21bcdfadf6269",
+    "url": "/Contact-Manager-React/static/css/main.5bf5a46e.chunk.css"
+  },
+  {
+    "revision": "a1992afb8ea0c927db08",
+    "url": "/Contact-Manager-React/static/css/1.a582037c.chunk.css"
+  },
+  {
+    "revision": "bf6205f2e0026a6e85c7938858547320",
+    "url": "/Contact-Manager-React/index.html"
+  }
+];
